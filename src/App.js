13610bbb72211c429d4e45893fbbf6e3ab1a5482
Sezer Ökmen <szrokmn34@gmail.com>
2022-12-05 12:39:07 +0300

    add components, helper and scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.scss";
 
-function App() {
-  return (
+const App = () => {
+  return(
     <div className="App">
-     
+      <h1>appp
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, iste?</p>
+      </h1>
     </div>
-  );
+  )
 }
 
 export default App;
