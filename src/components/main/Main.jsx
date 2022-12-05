4cc04072d5aa0460b9c23f992fb30619da4b300c
Sezer Ökmen <szrokmn34@gmail.com>
2022-12-05 17:49:26 +0300

    add Card.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,14 @@
 import "./Main.scss";
+import { data } from "../../helper/data";
+import Card from "./Card";
 
 const Main = () => {
+    console.log(data)
     return(
         <div className="main">
-            
+            {data.map((item, id)=>
+                <Card {...item} key={id}/>
+            )}
         </div>
     )
 }
